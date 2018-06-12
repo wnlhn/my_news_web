abf640a3be1dad420613721fc5bc1079609bcf97
wnlhn <wnlhn@sina.com>
2018-06-12 19:40:44 +0800

    添加model数据表结构文件和第三方的常量配置文件

--- main_info/modules/index/views.py
+++ main_info/modules/index/views.py
@@ -1,4 +1,5 @@
 # 问句天几高，心中志比天更高
+from flask import current_app
 from flask import render_template
 from main_info import redis_store
 from . import index_blue
@@ -18,4 +19,8 @@ def index():
     # logging.fatal('big_fatal')
     # current_app.logger.debug('current_app_debug')
     # 注意点: 标记了templates为templates_folder之后会自动进入查找
-    return render_template('news/other.html')
+    return render_template('news/index.html')
+
+@index_blue.route('/favicon.ico')
+def favicon_ico():
+    return current_app.send_static_file('news/favicon.ico')
