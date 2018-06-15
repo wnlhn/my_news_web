93d1e8ce2ee275e14c19a835f4ea73575a4565fc
wnlhn <wnlhn@sina.com>
2018-06-15 21:46:50 +0800

    详情页面对应的正文显示

--- main_info/modules/news/views.py
+++ main_info/modules/news/views.py
@@ -1,4 +1,5 @@
 # 问句天几高，心中志比天更高
+from flask import abort
 from flask import current_app, jsonify
 from flask import g
 from flask import render_template
@@ -34,10 +35,30 @@ def news_detail(news_id):
     except Exception as e:
         current_app.logger.error(e)
         return jsonify(errno=RET.DBERR,errmsg='数据库查询失败')
+    # 新闻对象列表转为字典列表,返回前端使用
+    news_to_list = []
+    for news in news_list:
+        news_to_list.append(news.to_dict())
+
+
+    # 显示正文内容实现
+    # 通过news_id查询news对象
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg='数据库查询失败')
+    if not news:
+        # 如果找不到就显示404错误，以后会对404页面进行特殊处理
+        abort(404)
+    # 将对象转化为列表方便前端使用
+
+
     data = {
         "user_info": g.user.to_dict() if g.user else None,
         "user_news_list": user_news_list,
         "user_fans": user_fans,
-        "news_list":news_list
+        "news_list":news_to_list,
+        "news_info":news.to_dict()
     }
     return render_template('news/detail.html',data=data)
